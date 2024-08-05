6001832e7fa74f1f9a64e9a461240f2e8aa14db1
Valter Garcia <valterlanegarcia@gmail.com>
2024-08-05 19:26:16 -0300

    Adicionando as rotas

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',             [PrincipalController::class, 'principal']);
+Route::get('/sobre-nos',   [SobreNosController::class, 'sobrenos']);
+Route::get('/contato',      [ContatoController::class, 'contato']);
+
+Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem}',
+ function(string  $nome,string $categoria,string $assunto,string $mensagem)
+{
+    echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
 });
+
+
+
